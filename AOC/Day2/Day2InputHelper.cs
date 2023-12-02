f1dcdab4717135e104d0ad09bdc31ee4440f333c
Iain Maclean <iain.p.m.maclean@gmail.com>
2023-12-02 09:15:09 +0000

    Solved Day 2

---
+++ AOC/Day2/Day2InputHelper.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+namespace AOC_2023.Day2
+{
+    internal class Day2InputHelper : InputHelper<Dictionary<int, List<(int r, int g, int b)>>>
+    {
+        public Day2InputHelper(string fileName) : base(fileName)
+        {
+        }
+
+        public override Dictionary<int, List<(int r, int g, int b)>> Parse()
+        {
+            var output = new Dictionary<int, List<(int r, int g, int b)>>();
+            using (var sr = new StreamReader(InputPath))
+            {
+                string ln;
+                var counter = 1;
+                var redRegex = new Regex(@"\d+(?= red)");
+                var greenRegex = new Regex(@"\d+(?= green)");
+                var blueRegex = new Regex(@"\d+(?= blue)");
+                while ((ln = sr.ReadLine()!) != null)
+                {
+                    var rounds = new List<(int r, int g, int b)>();
+                    var roundsRaw = ln.Split(':')[1].Split(';');
+                    foreach (var roundRaw in roundsRaw)
+                    {
+                        var rMatch = redRegex.Match(roundRaw);
+                        var gMatch = greenRegex.Match(roundRaw);
+                        var bMatch = blueRegex.Match(roundRaw);
+
+                        var r = rMatch.Success ? int.Parse(redRegex.Match(roundRaw).Value) : 0;
+                        var g = gMatch.Success ? int.Parse(greenRegex.Match(roundRaw).Value) : 0;
+                        var b = bMatch.Success ? int.Parse(blueRegex.Match(roundRaw).Value) : 0;
+
+                        rounds.Add((r, g, b));
+                    }
+                    output.Add(counter++, rounds);
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ AOC/Day2/Day2PuzzleManager.cs
@@ -0,0 +1,54 @@
+﻿namespace AOC_2023.Day2
+{
+    public class Day2PuzzleManager : PuzzleManager
+    {
+        public Dictionary<int, List<(int r, int g, int b)>> Input { get; set; }
+        public Day2PuzzleManager()
+        {
+            var inputHelper = new Day2InputHelper(INPUT_FILE_NAME);
+            Input = inputHelper.Parse();
+        }
+        public override Task SolveBothParts()
+        {
+            SolvePartOne();
+            Console.WriteLine();
+            SolvePartTwo();
+            return Task.CompletedTask;
+        }
+
+        public override Task SolvePartOne()
+        {
+            (int r, int g, int b) maxAllowed = (12, 13, 14);
+            var solution = 0;
+
+            for (var gameNumber = 1; gameNumber <= Input.Count; gameNumber++)
+            {
+                if (Input[gameNumber].All(x => 
+                    x.r <= maxAllowed.r &&
+                    x.g <= maxAllowed.g &
+                    x.b <= maxAllowed.b))
+                {
+                    solution += gameNumber;
+                }
+            }
+            Console.WriteLine($"The solution to part one is '{solution}'.");
+            return Task.CompletedTask;
+        }
+
+        public override Task SolvePartTwo()
+        {
+            var solution = 0;
+
+            foreach (var game in Input.Values)
+            {
+                var maxR = game.Max(x => x.r);
+                var maxG = game.Max(x => x.g);
+                var maxB = game.Max(x => x.b);
+
+                solution += maxR * maxG * maxB;
+            }
+            Console.WriteLine($"The solution to part two is '{solution}'.");
+            return Task.CompletedTask;
+        }
+    }
+}
