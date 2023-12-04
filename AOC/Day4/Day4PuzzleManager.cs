52b03bd0a8b7cb38fe748acbf217bd992d538d95
Iain Maclean <iain.p.m.maclean@gmail.com>
2023-12-04 09:27:28 +0000

    Solved Day 4.

---
+++ AOC/Day4/Day4PuzzleManager.cs
@@ -0,0 +1,59 @@
+﻿namespace AOC_2023.Day4
+{
+    public class Day4PuzzleManager : PuzzleManager
+    {
+        public List<Ticket> Input { get; set; }
+        public Day4PuzzleManager()
+        {
+            var inputHelper = new Day4InputHelper(INPUT_FILE_NAME);
+            Input = inputHelper.Parse();
+        }
+        public override Task SolveBothParts()
+        {
+            SolvePartOne();
+            Console.WriteLine();
+            SolvePartTwo();
+            return Task.CompletedTask;
+        }
+
+        public override Task SolvePartOne()
+        {
+            var solution = 0;
+            foreach (var ticket in Input)
+            {
+                var intersection = ticket.WinningNumbers.Intersect(ticket.OwnedNumbers);
+                solution += (int)Math.Pow(2, intersection.Count() - 1);
+            }
+            Console.WriteLine($"The solution to part one is '{solution}'.");
+            return Task.CompletedTask;
+        }
+
+        public override Task SolvePartTwo()
+        {
+            var ticketCounts = new Dictionary<int, int>();
+            for (var i = 1; i <= Input.Count; i++)
+            {
+                ticketCounts.Add(i, 1);
+            }
+
+            foreach (var ticketCountPair in ticketCounts)
+            {
+                if (ticketCountPair.Value == 0)
+                {
+                    continue;
+                }
+
+                var ticket = Input.First(x => x.CardNumber == ticketCountPair.Key);
+                var winCount = ticket.WinningNumbers.Intersect(ticket.OwnedNumbers).Count();
+
+                for (var i = 1; i <= winCount; i++)
+                {
+                    ticketCounts[ticketCountPair.Key + i] += ticketCountPair.Value;
+                }
+            }
+
+            Console.WriteLine($"The solution to part two is '{ticketCounts.Values.Sum()}'.");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ AOC/Day4/Ticket.cs
@@ -0,0 +1,16 @@
+﻿namespace AOC_2023.Day4
+{
+    public class Ticket
+    {
+        public int CardNumber { get; set; }
+        public HashSet<int> WinningNumbers { get; set; }
+        public HashSet<int> OwnedNumbers { get; set; }
+
+        public Ticket(int cardNumber, HashSet<int> winningNumbers, HashSet<int> ownedNumbers)
+        {
+            CardNumber = cardNumber;
+            WinningNumbers = winningNumbers;
+            OwnedNumbers = ownedNumbers;
+        }
+    }
+}
