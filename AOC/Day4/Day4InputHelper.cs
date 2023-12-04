6615aea822357fa52bc3feecc03e0237f4c082b9
ipmaclean <78161046+ipmaclean@users.noreply.github.com>
2023-12-04 09:27:52 +0000

    Solved Day 4. (#4)

---
+++ AOC/Day4/Day4InputHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AOC_2023.Day4
+{
+    internal class Day4InputHelper : InputHelper<List<Ticket>>
+    {
+        public Day4InputHelper(string fileName) : base(fileName)
+        {
+        }
+
+        public override List<Ticket> Parse()
+        {
+            var output = new List<Ticket>();
+            using (var sr = new StreamReader(InputPath))
+            {
+                string ln;
+                var regex = new Regex(@"\d+");
+                while ((ln = sr.ReadLine()!) != null)
+                {
+                    var splitLine = ln.Split(':')[1].Split('|');
+                    
+                    var winningMatches = regex.Matches(splitLine[0]);
+                    var ownedMatches = regex.Matches(splitLine[1]);
+
+                    var cardNumber = int.Parse(regex.Match(ln).Value);
+                    var winningNumbers = winningMatches.Select(x => int.Parse(x.Value)).ToHashSet();
+                    var ownedNumbers = ownedMatches.Select(x => int.Parse(x.Value)).ToHashSet();
+
+                    output.Add(new Ticket(cardNumber, winningNumbers, ownedNumbers));
+                }
+            }
+            return output;
+        }
+    }
+}
