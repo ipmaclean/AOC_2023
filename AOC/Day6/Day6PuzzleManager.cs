8a22bcfe0fc346fbd90b7194cf0e4396069e98c3
Iain Maclean <iain.p.m.maclean@gmail.com>
2023-12-06 15:57:08 +0000

    Solved Day 6

---
+++ AOC/Day6/Day6PuzzleManager.cs
@@ -0,0 +1,59 @@
+﻿namespace AOC_2023.Day6
+{
+    public class Day6PuzzleManager : PuzzleManager
+    {
+        public List<Race> Input { get; set; }
+        public List<Race> InputPartTwo { get; set; }
+        public Day6PuzzleManager()
+        {
+            var inputHelper = new Day6InputHelper(INPUT_FILE_NAME);
+            Input = inputHelper.Parse();
+            InputPartTwo = inputHelper.ParsePartTwo();
+        }
+        public override Task SolveBothParts()
+        {
+            SolvePartOne();
+            Console.WriteLine();
+            SolvePartTwo();
+            return Task.CompletedTask;
+        }
+
+        public override Task SolvePartOne()
+        {
+            Console.WriteLine($"The solution to part one is '{Solve(Input)}'.");
+            return Task.CompletedTask;
+        }
+
+
+        public override Task SolvePartTwo()
+        {
+            Console.WriteLine($"The solution to part two is '{Solve(InputPartTwo)}'.");
+            return Task.CompletedTask;
+        }
+        private int Solve(List<Race> input)
+        {
+            var solution = 1;
+            foreach (var race in input)
+            {
+                var discrim = Math.Sqrt(((race.Time * race.Time) / 4) - race.Distance);
+                var a = (race.Time / 2) + discrim;
+                var b = (race.Time / 2) - discrim;
+                var waysOfWinning = (int)Math.Floor(a) - (int)Math.Ceiling(b) + 1;
+                if (IsInteger(a))
+                {
+                    waysOfWinning--;
+                }
+                if (IsInteger(b))
+                {
+                    waysOfWinning--;
+                }
+                solution *= waysOfWinning;
+            }
+
+            return solution;
+        }
+
+        private bool IsInteger(double d)
+            => Math.Abs(d % 1) <= (double.Epsilon * 100);
+    }
+}
---
+++ AOC/Day6/Race.cs
@@ -0,0 +1,14 @@
+﻿namespace AOC_2023.Day6
+{
+    public class Race
+    {
+        public double Time { get; set; }
+        public double Distance { get; set; }
+
+        public Race(double time, double distance)
+        {
+            Time = time;
+            Distance = distance;
+        }
+    }
+}
