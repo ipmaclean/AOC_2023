92265301b9c840200e54aaca6702e130097c6fa5
ipmaclean <78161046+ipmaclean@users.noreply.github.com>
2023-12-06 15:57:30 +0000

    Solved Day 6 (#6)

---
+++ AOC/Day6/Day6InputHelper.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AOC_2023.Day6
+{
+    public class Day6InputHelper : InputHelper<List<Race>>
+    {
+        public Day6InputHelper(string fileName) : base(fileName)
+        {
+        }
+
+        public override List<Race> Parse()
+        {
+            var numberRegex = new Regex(@"\d+");
+            var times = new List<double>();
+            var distances = new List<double>();
+            using (var sr = new StreamReader(InputPath))
+            {
+                times = numberRegex.Matches(sr.ReadLine()!).Select(x => double.Parse(x.Value)).ToList();
+                distances = numberRegex.Matches(sr.ReadLine()!).Select(x => double.Parse(x.Value)).ToList();
+            }
+            var output = new List<Race>();
+            for (var i = 0; i < times.Count; i++)
+            {
+                output.Add(new Race(times[i], distances[i]));
+            }
+            return output;
+        }
+
+        public List<Race> ParsePartTwo()
+        {
+            var numberRegex = new Regex(@"\d+");
+            var output = new List<Race>();
+            using (var sr = new StreamReader(InputPath))
+            {
+                var time = double.Parse(numberRegex.Matches(sr.ReadLine()!).Select(x => x.Value).ToList().Aggregate(string.Empty, (x, y) => x + y));
+                var distance = double.Parse(numberRegex.Matches(sr.ReadLine()!).Select(x => x.Value).ToList().Aggregate(string.Empty, (x, y) => x + y));
+                output.Add(new Race(time, distance));
+            }
+            return output;
+        }
+    }
+}
