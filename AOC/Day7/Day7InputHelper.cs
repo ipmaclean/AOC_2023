151bb20c9b6a77701e4572dcf03eb3f6ed0cd2d7
Iain Maclean <iain.p.m.maclean@gmail.com>
2023-12-07 16:39:53 +0000

    Solved Day 7

---
+++ AOC/Day7/Day7InputHelper.cs
@@ -0,0 +1,39 @@
+﻿namespace AOC_2023.Day7
+{
+    public class Day7InputHelper : InputHelper<List<Hand>>
+    {
+        public Day7InputHelper(string fileName) : base(fileName)
+        {
+        }
+
+        public override List<Hand> Parse()
+        {
+            var output = new List<Hand>();
+            using (var sr = new StreamReader(InputPath))
+            {
+                string ln;
+                while ((ln = sr.ReadLine()!) != null)
+                {
+                    var split = ln.Split(' ');
+                    output.Add(new Hand(split[0], long.Parse(split[1])));
+                }
+            }
+            return output;
+        }
+
+        public List<HandPartTwo> ParsePartTwo()
+        {
+            var output = new List<HandPartTwo>();
+            using (var sr = new StreamReader(InputPath))
+            {
+                string ln;
+                while ((ln = sr.ReadLine()!) != null)
+                {
+                    var split = ln.Split(' ');
+                    output.Add(new HandPartTwo(split[0], long.Parse(split[1])));
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ AOC/Day7/Day7PuzzleManager.cs
@@ -0,0 +1,47 @@
+﻿namespace AOC_2023.Day7
+{
+    public class Day7PuzzleManager : PuzzleManager
+    {
+        public List<Hand> InputPartOne { get; set; }
+        public List<HandPartTwo> InputPartTwo { get; set; }
+        public Day7PuzzleManager()
+        {
+            var inputHelper = new Day7InputHelper(INPUT_FILE_NAME);
+            InputPartOne = inputHelper.Parse();
+            InputPartTwo = inputHelper.ParsePartTwo();
+        }
+        public override Task SolveBothParts()
+        {
+            SolvePartOne();
+            Console.WriteLine();
+            SolvePartTwo();
+            return Task.CompletedTask;
+        }
+
+        public override Task SolvePartOne()
+        {
+            var hands = new List<Hand>(InputPartOne);
+            hands.Sort();
+            var solution = 0L;
+            for (var i = 0; i < hands.Count; i++)
+            {
+                solution += hands[i].Bid * ((long)i + 1);
+            }
+            Console.WriteLine($"The solution to part one is '{solution}'.");
+            return Task.CompletedTask;
+        }
+
+        public override Task SolvePartTwo()
+        {
+            var hands = new List<HandPartTwo>(InputPartTwo);
+            hands.Sort();
+            var solution = 0L;
+            for (var i = 0; i < hands.Count; i++)
+            {
+                solution += hands[i].Bid * ((long)i + 1);
+            }
+            Console.WriteLine($"The solution to part two is '{solution}'.");
+            return Task.CompletedTask;
+        }
+    }
+}
