c918bd13d85e1ac64c957619a4b6f6ab5da28bef
Iain Maclean <iain.p.m.maclean@gmail.com>
2023-12-08 09:13:29 +0000

    Solved Day 8

---
+++ AOC/Day8/Day8InputHelper.cs
@@ -0,0 +1,32 @@
+﻿namespace AOC_2023.Day8
+{
+    public class Day8InputHelper : InputHelper<(string, List<Node>)>
+    {
+        public Day8InputHelper(string fileName) : base(fileName)
+        {
+        }
+
+        public override (string, List<Node>) Parse()
+        {
+            var directions = string.Empty;
+            var nodes = new List<Node>();
+            using (var sr = new StreamReader(InputPath))
+            {
+                directions = sr.ReadLine()!;
+
+                var ln = sr.ReadLine()!;
+                while ((ln = sr.ReadLine()!) != null)
+                {
+                    nodes.Add(new Node(ln.Substring(0, 3), ln.Substring(7, 3), ln.Substring(12, 3)));
+                }
+
+                foreach (var node in nodes)
+                {
+                    node.Left = nodes.FirstOrDefault(x => x.Name == node.LeftName);
+                    node.Right = nodes.FirstOrDefault(x => x.Name == node.RightName);
+                }
+            }
+            return (directions, nodes);
+        }
+    }
+}
