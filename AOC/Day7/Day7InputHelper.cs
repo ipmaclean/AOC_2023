ab7ca711c63fac57b78485a32393955eeabdb683
Iain Maclean <iain.p.m.maclean@gmail.com>
2023-12-08 09:19:25 +0000

    Neatened up Day 7 a bit

--- AOC/Day7/Day7InputHelper.cs
+++ AOC/Day7/Day7InputHelper.cs
@@ -15,22 +15,22 @@
                 while ((ln = sr.ReadLine()!) != null)
                 {
                     var split = ln.Split(' ');
-                    output.Add(new Hand(split[0], long.Parse(split[1])));
+                    output.Add(new Hand(split[0], long.Parse(split[1]), 1));
                 }
             }
             return output;
         }
 
-        public List<HandPartTwo> ParsePartTwo()
+        public List<Hand> ParsePartTwo()
         {
-            var output = new List<HandPartTwo>();
+            var output = new List<Hand>();
             using (var sr = new StreamReader(InputPath))
             {
                 string ln;
                 while ((ln = sr.ReadLine()!) != null)
                 {
                     var split = ln.Split(' ');
-                    output.Add(new HandPartTwo(split[0], long.Parse(split[1])));
+                    output.Add(new Hand(split[0], long.Parse(split[1]), 2));
                 }
             }
             return output;
--- AOC/Day7/Day7PuzzleManager.cs
+++ AOC/Day7/Day7PuzzleManager.cs
@@ -3,7 +3,7 @@
     public class Day7PuzzleManager : PuzzleManager
     {
         public List<Hand> InputPartOne { get; set; }
-        public List<HandPartTwo> InputPartTwo { get; set; }
+        public List<Hand> InputPartTwo { get; set; }
         public Day7PuzzleManager()
         {
             var inputHelper = new Day7InputHelper(INPUT_FILE_NAME);
@@ -21,27 +21,26 @@
         public override Task SolvePartOne()
         {
             var hands = new List<Hand>(InputPartOne);
-            hands.Sort();
-            var solution = 0L;
-            for (var i = 0; i < hands.Count; i++)
-            {
-                solution += hands[i].Bid * ((long)i + 1);
-            }
-            Console.WriteLine($"The solution to part one is '{solution}'.");
+            Console.WriteLine($"The solution to part one is '{Solve(hands)}'.");
             return Task.CompletedTask;
         }
 
         public override Task SolvePartTwo()
         {
-            var hands = new List<HandPartTwo>(InputPartTwo);
+            var hands = new List<Hand>(InputPartTwo);
+            Console.WriteLine($"The solution to part two is '{Solve(hands)}'.");
+            return Task.CompletedTask;
+        }
+
+        private static long Solve(List<Hand> hands)
+        {
             hands.Sort();
             var solution = 0L;
             for (var i = 0; i < hands.Count; i++)
             {
                 solution += hands[i].Bid * ((long)i + 1);
             }
-            Console.WriteLine($"The solution to part two is '{solution}'.");
-            return Task.CompletedTask;
+            return solution;
         }
     }
 }
