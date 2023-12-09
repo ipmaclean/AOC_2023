73d19f1e35adbaf10a3dabe5fe05d38c82a4f58d
Iain Maclean <iain.p.m.maclean@gmail.com>
2023-12-09 09:11:23 +0000

    Solved Day 9

---
+++ AOC/Day9/Day9InputHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AOC_2023.Day9
+{
+    public class Day9InputHelper : InputHelper<List<List<long>>>
+    {
+        public Day9InputHelper(string fileName) : base(fileName)
+        {
+        }
+
+        public override List<List<long>> Parse()
+        {
+            var redRegex = new Regex(@"-*\d+");
+            var output = new List<List<long>>();
+            using (var sr = new StreamReader(InputPath))
+            {
+                string ln;
+                while ((ln = sr.ReadLine()!) != null)
+                {
+                    output.Add(redRegex.Matches(ln).Select(x => long.Parse(x.Value)).ToList());
+                }
+            }
+            return output;
+        }
+    }
+}
