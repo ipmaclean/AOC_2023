3e795e85a0c48863b59eb532c243eca975ab2113
Iain Maclean <iain.p.m.maclean@gmail.com>
2023-12-11 10:07:24 +0000

    Solved Day 11

---
+++ AOC/Day11/Day11InputHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace AOC_2023.Day11
+{
+    public class Day11InputHelper : InputHelper<List<string>>
+    {
+        public Day11InputHelper(string fileName) : base(fileName)
+        {
+        }
+
+        public override List<string> Parse()
+        {
+            var output = new List<string>();
+            using (var sr = new StreamReader(InputPath))
+            {
+                string ln;
+                while ((ln = sr.ReadLine()!) != null)
+                {
+                    output.Add(ln);
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ AOC/Day11/Galaxy.cs
@@ -0,0 +1,14 @@
+﻿namespace AOC_2023.Day11
+{
+    public class Galaxy
+    {
+        public long X { get; set; }
+        public long Y { get; set; }
+
+        public Galaxy(long x, long y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
