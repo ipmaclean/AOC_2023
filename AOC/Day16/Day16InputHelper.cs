b34b093653d648c508d85bda3d3387b5deb2d860
ipmaclean <78161046+ipmaclean@users.noreply.github.com>
2023-12-16 11:00:02 +0000

    Solved Day 16 (#16)

---
+++ AOC/Day16/Day16InputHelper.cs
@@ -0,0 +1,33 @@
+﻿namespace AOC_2023.Day16
+{
+    public class Day16InputHelper : InputHelper<Tile[,]>
+    {
+        public Day16InputHelper(string fileName) : base(fileName)
+        {
+        }
+
+        public override Tile[,] Parse()
+        {
+            var output = new Tile[0, 0];
+            using (var sr = new StreamReader(InputPath))
+            {
+                var ln = sr.ReadLine()!;
+                output = new Tile[ln.Length, ln.Length];
+                var y = 0;
+
+                while (ln != null)
+                {
+                    for (var x = 0; x < output.GetLength(0); x++)
+                    {
+                        output[x, y] = new Tile(ln[x]);
+                    }
+                    y++;
+                    ln = sr.ReadLine();
+                }
+            }
+            return output;
+        }
+
+
+    }
+}
---
+++ AOC/Day16/Tile.cs
@@ -0,0 +1,13 @@
+﻿namespace AOC_2023.Day16
+{
+    public class Tile
+    {
+        public char Type { get; set; }
+        public HashSet<Direction> LightDirections { get; set; } = new HashSet<Direction>();
+
+        public Tile(char type)
+        {
+            Type = type;
+        }
+    }
+}
