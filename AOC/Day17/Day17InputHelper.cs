333a36761552d30c979df88165bd5d29dd720aca
Iain Maclean <iain.p.m.maclean@gmail.com>
2023-12-17 09:05:32 +0000

    Solved Part 1

---
+++ AOC/Day17/Day17InputHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace AOC_2023.Day17
+{
+    public class Day17InputHelper : InputHelper<int[,]>
+    {
+        public Day17InputHelper(string fileName) : base(fileName)
+        {
+        }
+
+        public override int[,] Parse()
+        {
+            var output = new int[0, 0];
+            using (var sr = new StreamReader(InputPath))
+            {
+                var ln = sr.ReadLine()!;
+                output = new int[ln.Length, ln.Length];
+                var y = 0;
+
+                while (ln != null)
+                {
+                    for (var x = 0; x < output.GetLength(0); x++)
+                    {
+                        output[x, y] = ln[x] - '0';
+                    }
+                    y++;
+                    ln = sr.ReadLine();
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ AOC/Day17/Day17PuzzleManager.cs
@@ -0,0 +1,105 @@
+﻿using BfsStateNoDistance = (int x, int y, AOC_2023.Day17.Direction direction, int distanceTravelledStraight);
+using BfsState = (int x, int y, AOC_2023.Day17.Direction direction, int distanceTravelledStraight, int totalDistance);
+
+namespace AOC_2023.Day17
+{
+    public class Day17PuzzleManager : PuzzleManager
+    {
+        public int[,] Input { get; set; }
+        public Day17PuzzleManager()
+        {
+            var inputHelper = new Day17InputHelper(INPUT_FILE_NAME);
+            Input = inputHelper.Parse();
+        }
+        public override Task SolveBothParts()
+        {
+            SolvePartOne();
+            Console.WriteLine();
+            SolvePartTwo();
+            return Task.CompletedTask;
+        }
+
+        public override Task SolvePartOne()
+        {
+            Console.WriteLine($"The solution to part one is '{FindShortestPath(Input)}'.");
+            return Task.CompletedTask;
+        }
+
+        private int FindShortestPath(int[,] input)
+        {
+            // Because we can't discount visited nodes, we can't use Dijkstra.
+            // We'll instead use a priority queue BFS with a state including
+            // distance travelled straight and direction (along with total distance).
+            var states = new PriorityQueue<BfsState, int>();
+            states.Enqueue((0, 0, Direction.East, 0, 0), 0);
+            var visitedStates = new HashSet<BfsStateNoDistance>();
+
+            while (states.Count > 0)
+            {
+                var currentState = states.Dequeue();
+
+                // Enqueue next states if we have not yet seen them
+                List<BfsStateNoDistance> nextStates = currentState.direction switch
+                {
+                    Direction.North => new List<BfsStateNoDistance>() {
+                        (currentState.x - 1, currentState.y, Direction.West, 1),
+                        (currentState.x, currentState.y - 1, Direction.North, currentState.distanceTravelledStraight + 1),
+                        (currentState.x + 1, currentState.y, Direction.East, 1)
+                    },
+                    Direction.East => new List<BfsStateNoDistance>() {
+                        (currentState.x, currentState.y - 1, Direction.North, 1),
+                        (currentState.x + 1, currentState.y, Direction.East, currentState.distanceTravelledStraight + 1),
+                        (currentState.x, currentState.y + 1, Direction.South, 1)
+                    },
+                    Direction.South => new List<BfsStateNoDistance>() {
+                        (currentState.x - 1, currentState.y, Direction.West, 1),
+                        (currentState.x, currentState.y + 1, Direction.South, currentState.distanceTravelledStraight + 1),
+                        (currentState.x + 1, currentState.y, Direction.East, 1)
+                    },
+                    Direction.West => new List<BfsStateNoDistance>() {
+                        (currentState.x, currentState.y - 1, Direction.North, 1),
+                        (currentState.x - 1, currentState.y, Direction.West, currentState.distanceTravelledStraight + 1),
+                        (currentState.x, currentState.y + 1, Direction.South, 1)
+                    },
+                    _ => throw new ArgumentOutOfRangeException("Unexpected Light Direction found.")
+                };
+
+                // Check that states are valid and not already visited then
+                // add the the queue of states to visit.
+                foreach (var nextState in nextStates)
+                {
+                    if (nextState.distanceTravelledStraight > 3)
+                    {
+                        continue;
+                    }
+                    if (nextState.x < 0 || nextState.x >= input.GetLength(0) ||
+                        nextState.y < 0 || nextState.y >= input.GetLength(0))
+                    {
+                        continue;
+                    }
+                    if (visitedStates.Contains(nextState))
+                    {
+                        continue;
+                    }
+                    if (nextState.x == input.GetLength(0) - 1 && nextState.y == input.GetLength(0) - 1)
+                    {
+                        return currentState.totalDistance + input[nextState.x, nextState.y];
+                    }
+                    visitedStates.Add(nextState);
+                    states.Enqueue(
+                        (nextState.x, nextState.y, nextState.direction, nextState.distanceTravelledStraight, currentState.totalDistance + input[nextState.x, nextState.y]),
+                        currentState.totalDistance + input[nextState.x, nextState.y]
+                        );
+                }
+            }
+            return -1;
+        }
+
+        public override Task SolvePartTwo()
+        {
+            var solution = 0;
+            Console.WriteLine($"The solution to part two is '{solution}'.");
+            return Task.CompletedTask;
+        }
+    }
+}
