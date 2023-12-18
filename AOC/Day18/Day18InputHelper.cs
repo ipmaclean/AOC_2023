77a64bfc256a8b290fd720e4a0aac4c96b952cee
Iain Maclean <iain.p.m.maclean@gmail.com>
2023-12-18 07:13:46 +0000

    Solved Day 18

---
+++ AOC/Day18/Day18InputHelper.cs
@@ -0,0 +1,24 @@
+﻿namespace AOC_2023.Day18
+{
+    public class Day18InputHelper : InputHelper<List<DigInstruction>>
+    {
+        public Day18InputHelper(string fileName) : base(fileName)
+        {
+        }
+
+        public override List<DigInstruction> Parse()
+        {
+            var output = new List<DigInstruction>();
+            using (var sr = new StreamReader(InputPath))
+            {
+                string ln;
+                while ((ln = sr.ReadLine()!) != null)
+                {
+                    var split = ln.Split(' ');
+                    output.Add(new DigInstruction(split[0][0], long.Parse(split[1]), split[2].Substring(2, 6)));
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ AOC/Day18/DigInstruction.cs
@@ -0,0 +1,16 @@
+﻿namespace AOC_2023.Day18
+{
+    public class DigInstruction
+    {
+        public char Direction { get; set; }
+        public long Distance { get; set; }
+        public string ColourHex { get; set; }
+
+        public DigInstruction(char direction, long amount, string colourHex)
+        {
+            Direction = direction;
+            Distance = amount;
+            ColourHex = colourHex;
+        }
+    }
+}
